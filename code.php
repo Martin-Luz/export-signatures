2cc782648e9b4d91b358d1ac4ef88234db1335e1
Kauê Holthausen <kaueholt@gmail.com>
2022-03-04 16:11:57 -0300

    Atualização do layout

--- code.php
+++ code.php
@@ -1,4 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
 
+<head>
+	<!-- Meta tags -->
+	<meta charset="utf-8" />
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+
+
+<body style="height: 100vh">
+	<div class="d-flex h-100 justify-content-center">
+		<?php
+		// shell_exec('if exist %APPDATA%\Microsoft\Assinaturas\ ( explorer %APPDATA%\Microsoft\Assinaturas\ ) else ( explorer %APPDATA%\Microsoft\Signatures\ )'); 
+		?>
+		<!-- localhost debug  -->
+		<!-- <section id="codeHtml" class="align-self-center"> 
+			<a href="https://cyclopay.com" style=" display: inline-block" target="_blank">
+				<img id="logo" src="//localhost/emailPNG/logoby.png" width="370px" style="width: 370px; margin-right: 42px; " />
+			</a>
+			<div style="display: inline-block; vertical-align: middle; height: 120px; padding: 10px 0 10px 35px; border-left: 1px solid #ccc; ">
+				<h3 class="name" style="color: #1536be; font-size: 20px; font-family: Arial; font-family: Arial; font-weight: 800; text-transform: capitalize; margin-bottom: 0px"></h3>
+				<h4 class="job" style="color: #E62DFC; font-size: 14px; font-family: Arial;  font-style: italic; font-weight: 600; text-transform: capitalize; margin-bottom: 0px"></h4>
+				<a href="#" style="display: inline-block; margin-bottom: 3px; text-decoration: none;  color: #4e4b5b" target="_blank">
+					<img id="whats" src="//localhost/emailPNG/whatsapp.png" width="36px" style="width: 36px; " />
+					<h4 id="tel" style="display: inline-block; font-size: 14px; font-family: Arial; font-family: Arial; font-weight: 600; ">Tel: +55 (11) 94534-9787</h4>
+				</a>
+				<br />
+				<img id="whats" src="//localhost/emailPNG/SOCIAL-01.png" width="34px" style="width: 34px; margin-right: -10px" />
+				<img id="whats" src="//localhost/emailPNG/SOCIAL-03.png" width="34px" style="width: 34px; margin-right: -7px" />
+				<img id="whats" src="//localhost/emailPNG/SOCIAL-02.png" width="34px" style="width: 34px; margin-right: -7px" />
+				<img id="whats" src="//localhost/emailPNG/SOCIAL-04.png" width="34px" style="width: 34px; " />
+			</div>
+		</section> -->
+
+		<section id="codeHtml" class="align-self-center">
+			<a href="https://cyclopay.com" style=" display: inline-block" target="_blank">
+				<img id="logo" src="http://cyclopay.martinluz.com.br/site/email/logoby.png" width="370px" style="width: 370px; margin-right: 42px; " />
+			</a>
+			<div style="display: inline-block; vertical-align: middle; height: 120px; padding: 10px 0 10px 35px; border-left: 1px solid #ccc; ">
+				<h3 class="name" style="color: #1536be; font-size: 20px; font-family: Arial; font-family: Arial; font-weight: 800; text-transform: capitalize; margin-bottom: 0px"></h3>
+				<h4 class="job" style="color: #E62DFC; font-size: 14px; font-family: Arial;  font-style: italic; font-weight: 600; text-transform: capitalize; margin-bottom: 0px"></h4>
+				<a href="#" style="display: inline-block; margin-bottom: 3px; text-decoration: none;  color: #4e4b5b" target="_blank">
+					<img id="whats" src="http://cyclopay.martinluz.com.br/site/email/whatsapp.png" width="36px" style="width: 36px; " />
+					<h4 id="tel" style="display: inline-block; font-size: 14px; font-family: Arial; font-family: Arial; font-weight: 600; ">Tel: +55 (11) 94534-9787</h4>
+				</a>
+				<br />
+				<img id="whats" src="http://cyclopay.martinluz.com.br/site/email/SOCIAL-01.png" width="34px" style="width: 34px; margin-right: -10px" />
+				<img id="whats" src="http://cyclopay.martinluz.com.br/site/email/SOCIAL-03.png" width="34px" style="width: 34px; margin-right: -7px" />
+				<img id="whats" src="http://cyclopay.martinluz.com.br/site/email/SOCIAL-02.png" width="34px" style="width: 34px; margin-right: -7px" />
+				<img id="whats" src="http://cyclopay.martinluz.com.br/site/email/SOCIAL-04.png" width="34px" style="width: 34px; " />
+			</div>
+		</section>
+
+		<!-- são necessários dois blocos, pois as widths das imagens não 
+		correspondem no html e Outlook. Acima, o código html (exibido), 
+		abaixo, o código editado via plataforma Outlook (exportado) -->
+
+		<!-- também é válido lembrar que o boostrap aplicado não será mantido na exportação -->
+
+		<main class="align-self-center d-none">
 			<table class="MsoNormalTable" style="
 				mso-cellspacing: 1.5pt;
 				mso-yfti-tbllook: 1184;
@@ -16,7 +79,7 @@
 								<span style="mso-fareast-font-family: 'Arial'">
 									<a href="https://cyclopay.com/" target="_blank">
 										<span style="text-decoration: none; text-underline: none">
-											<img id="_x0000_i1031" src="http://cyclopay.martinluz.com.br/site/email/logoby.png" style="margin-right: 42px; width: 370px" width="460" height="260" border="0">
+											<img id="_x0000_i1031" src="http://cyclopay.martinluz.com.br/site/email/logoby.png" style="margin-right: 42px; width: 370px" width="460" height="160" border="0">
 										</span>
 									</a>
 									<o:p></o:p>
@@ -35,7 +98,7 @@
 									<span id="name" style="
 									mso-fareast-font-family: 'Arial';
 									color: #1536be;
-									font-size: 20px;
+									font-size: px;
 									color: #1536be;
 									font-family: Arial;
 									font-weight: 800;
@@ -49,7 +112,7 @@
 										font-family: 'Arial', sans-serif;
 										mso-fareast-font-family: 'Arial';
 										mso-fareast-font-family: 'Arial';
-										color: #a00aaf;
+										color: #E62DFC;
 										font-size: 14px;
 										font-style: italic;
 										font-weight: 600;
@@ -75,8 +138,8 @@
 										mso-fareast-font-family: 'Arial';
 										color: #4e4b5b;
 										">
-										<a href="tel:5511945349787" target="_blank">
-											<span style="color: #4e4b5b">tel: +55 (11) 94534-9787
+										<a href="tel:5511945349787" target="_blank" style="color: #4e4b5b; text-decoration: none;">
+											<span style="color: #4e4b5b; text-decoration: none;">Tel: +55 (11) 94534-9787
 											</span>
 										</a>
 									</span>
@@ -109,4 +172,175 @@
 					</tr>
 				</tbody>
 			</table>
-		
+		</main>
+
+	</div>
+	<div id="info" class="p-4">
+		<h5>Em seu Outlook, navegue para "Novo Email > Assinatura > Assinaturas > Novo".
+			<br />
+			Adicione um nome à sua assinatura. Aperte "OK".
+			<br />
+			<br />
+			Na pasta %APPDATA%\Microsoft\Signatures ( ou Assinaturas), edite o arquivo <i>".htm"</i> que tem o mesmo nome da assinatura que já foi criada.
+			<br />
+			Selecione todo o conteúdo do arquivo (ctrl + A), e apague.
+			<br />
+			<br />
+			Copie o código abaixo, e cole dentro deste arquivo. Salve e feche.
+		</h5>
+	</div>
+	<button class="copiar m-4 btn btn-primary" onclick="copyToClipboard($('code').text())">Copiar</button>
+
+	<pre><code></code></pre>
+
+	<script src="./jquery3.js"></script>
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+	<script type="text/javascript">
+		function copyToClipboard(textToCopy) {
+			// console.log(textToCopy)
+			// navigator clipboard api needs a secure context (https)
+			if (navigator.clipboard && window.isSecureContext) {
+				// navigator clipboard api method'
+				return navigator.clipboard.writeText(textToCopy) && alert('Copiado!');
+			} else {
+				// text area method
+				let textArea = document.createElement("textarea");
+				textArea.value = textToCopy;
+				// make the textarea out of viewport
+				textArea.style.position = "fixed";
+				textArea.style.left = "-999999px";
+				textArea.style.top = "-999999px";
+				document.body.appendChild(textArea);
+				textArea.focus();
+				textArea.select();
+				return new Promise((res, rej) => {
+					// here the magic happens
+					document.execCommand('copy') ? res() : rej();
+					textArea.remove();
+				});
+			}
+		}
+
+		jQuery(window).ready(function($) {
+			var url_string = window.location.href;
+			var url = new URL(url_string);
+			var nome = url.searchParams.get("nome");
+			var cargo = url.searchParams.get("cargo");
+
+			displayCodeOnScreen = function() {
+				var code = $("code");
+				var main = $("main");
+				var mainInner = main.prop("innerHTML");
+				code.text(mainInner);
+				$('body,html').delay(2000).animate({
+					scrollTop: $("#info").offset().top
+				}, 1000);
+			};
+
+			gerar = function() {
+				// imageToMatrix('logo'); 
+				$("#name, #codeHtml .name").text(nome);
+				$("#job, #codeHtml .job").text(cargo);
+				displayCodeOnScreen();
+			};
+			gerar();
+
+			function convertImageToBase64() {
+				base_image = new Image();
+				base_image.src = './logoby.png';
+				console.log(base_image);
+				// canvas.width = $(window).width(); //change the canvas size
+				// canvas.height = $(window).height(); 
+				document.getElementById('canvas').getContext('2d').drawImage(base_image, 0, 0);
+				var image = canvas.toDataURL("image/png") // here is the most important part because if you dont replace you will get a DOM 18 exception.
+				// var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); // here is the most important part because if you dont replace you will get a DOM 18 exception.
+				console.log(image);
+			}
+			convertImageToBase64();
+
+			// imageToCanvas = function() {
+			// 	var canvas = document.getElementById('canvas'); 
+			// 	var context = canvas.getContext('2d'); 
+
+			// 	function make_base() {
+			// 		base_image = new Image(); 
+			// 		base_image.src = './ML.jpg'; 
+			// 		console.log(base_image); 
+			// 		// canvas.width = $(window).width(); //change the canvas size
+			// 		// canvas.height = $(window).height(); 
+			// 		context.drawImage(base_image, 0, 0); 
+			// 		var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); // here is the most important part because if you dont replace you will get a DOM 18 exception.
+			// 		console.log(image); 
+			// 		canvas = image; 
+			// 	}
+			// 	make_base(); 
+
+			// }
+			// imageToCanvas(); 
+
+			// var fileURLs = ['./SOCIAL-01.png', './SOCIAL-02.png', './SOCIAL-03.png', './SOCIAL-04.png', './whatsapp.png', './logoby.png']; 
+			// var zip = new JSZip(); 
+			// var count = 0; 
+
+			// onDownloadComplete = function() {
+			// 	console.log(1)
+			// }
+			// downloadFile(fileURLs[count], onDownloadComplete); 
+
+			// function downloadFile(url, onSuccess) {
+			// 	var xhr = new XMLHttpRequest(); 
+			// 	xhr.open('GET', url, true); 
+			// 	xhr.responseType = "blob"; 
+			// 	xhr.onreadystatechange = function() {
+			// 		if (xhr.readyState == 4) {
+			// 			if (onSuccess) onSuccess(xhr.response); 
+			// 		}
+
+			// 		function onDownloadComplete(blobData) {
+			// 			if (count < fileURLs.length) {
+			// 				blobToBase64(blobData, function(binaryData) {
+			// 					// add downloaded file to zip:
+			// 					var fileName = fileURLs[count].substring(fileURLs[count].lastIndexOf('/') + 1); 
+			// 					zip.file(fileName, binaryData, {
+			// 						base64: true
+			// 					}); 
+			// 					if (count < fileURLs.length - 1) {
+			// 						count++; 
+			// 						downloadFile(fileURLs[count], onDownloadCompleted); 
+			// 					} else {
+			// 						// all files have been downloaded, create the zip
+			// 						var content = zip.generate(); 
+
+			// 						// then trigger the download link:    
+			// 						var zipName = 'download.zip'; 
+			// 						var a = document.createElement('a'); 
+			// 						a.href = "data:application/zip; base64," + content; 
+			// 						a.download = zipName; 
+			// 						a.click(); 
+			// 					}
+			// 				}); 
+			// 			}
+			// 		}
+
+			// 		function blobToBase64(blob, callback) {
+			// 			var reader = new FileReader(); 
+			// 			reader.onload = function() {
+			// 				var dataUrl = reader.result; 
+			// 				var base64 = dataUrl.split(',')[1]; 
+			// 				callback(base64); 
+			// 			}; 
+			// 			reader.readAsDataURL(blob); 
+			// 		}
+
+			// 		function calculateAndUpdateProgress(evt) {
+			// 		}
+			// 	}
+			// }
+		});
+	</script>
+
+</body>
+
+</html>
